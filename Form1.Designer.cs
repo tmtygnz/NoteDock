af0b52c1d3c2e3d4d9aa103e3d70bc8842f85d6e
Timothy Ganoza <jostimian@gmail.com>
2020-11-10 17:42:20 +0800

    initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace NoteDock
+{
+	partial class NoteDock
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NoteDock));
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.newToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.loading = new System.Windows.Forms.ToolStripProgressBar();
+			this.FilePath = new System.Windows.Forms.ToolStripStatusLabel();
+			this.aboutNoteDockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.Note = new System.Windows.Forms.RichTextBox();
+			this.contextMenuStrip1.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.newToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(113, 92);
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+			this.saveToolStripMenuItem.Text = "Save";
+			// 
+			// saveAsToolStripMenuItem
+			// 
+			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+			this.saveAsToolStripMenuItem.Text = "Save as";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+			this.openToolStripMenuItem.Text = "Open";
+			// 
+			// newToolStripMenuItem
+			// 
+			this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+			this.newToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+			this.newToolStripMenuItem.Text = "New";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem1,
+            this.openToolStripMenuItem1,
+            this.saveToolStripMenuItem1,
+            this.saveAsToolStripMenuItem1});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutNoteDockToolStripMenuItem});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Text = "Help";
+			// 
+			// newToolStripMenuItem1
+			// 
+			this.newToolStripMenuItem1.Name = "newToolStripMenuItem1";
+			this.newToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+			this.newToolStripMenuItem1.Text = "New";
+			this.newToolStripMenuItem1.Click += new System.EventHandler(this.newToolStripMenuItem1_Click);
+			// 
+			// openToolStripMenuItem1
+			// 
+			this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+			this.openToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+			this.openToolStripMenuItem1.Text = "Open";
+			this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+			// 
+			// saveToolStripMenuItem1
+			// 
+			this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+			this.saveToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+			this.saveToolStripMenuItem1.Text = "Save";
+			this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
+			// 
+			// saveAsToolStripMenuItem1
+			// 
+			this.saveAsToolStripMenuItem1.Name = "saveAsToolStripMenuItem1";
+			this.saveAsToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+			this.saveAsToolStripMenuItem1.Text = "Save as";
+			this.saveAsToolStripMenuItem1.Click += new System.EventHandler(this.saveAsToolStripMenuItem1_Click);
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loading,
+            this.FilePath});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+			this.statusStrip1.TabIndex = 2;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// loading
+			// 
+			this.loading.Name = "loading";
+			this.loading.Size = new System.Drawing.Size(100, 16);
+			this.loading.Step = 5;
+			// 
+			// FilePath
+			// 
+			this.FilePath.Name = "FilePath";
+			this.FilePath.Size = new System.Drawing.Size(52, 17);
+			this.FilePath.Text = "New File";
+			// 
+			// aboutNoteDockToolStripMenuItem
+			// 
+			this.aboutNoteDockToolStripMenuItem.Name = "aboutNoteDockToolStripMenuItem";
+			this.aboutNoteDockToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.aboutNoteDockToolStripMenuItem.Text = "About Note Dock";
+			// 
+			// Note
+			// 
+			this.Note.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.Note.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Note.Location = new System.Drawing.Point(0, 27);
+			this.Note.Name = "Note";
+			this.Note.Size = new System.Drawing.Size(800, 398);
+			this.Note.TabIndex = 4;
+			this.Note.Text = "";
+			this.Note.TextChanged += new System.EventHandler(this.Note_TextChanged);
+			// 
+			// NoteDock
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Note);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "NoteDock";
+			this.Text = "NoteDock";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.NoteDock_FormClosing);
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripProgressBar loading;
+		private System.Windows.Forms.ToolStripMenuItem aboutNoteDockToolStripMenuItem;
+		private System.Windows.Forms.RichTextBox Note;
+		internal System.Windows.Forms.ToolStripStatusLabel FilePath;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteDock
+{
+	public partial class NoteDock : Form
+	{
+
+		string path = @"";
+		bool saved = true;
+		public NoteDock()
+		{
+			InitializeComponent();
+		}
+		private void newToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			loading.Value = 100;
+			SaveFileDialog save = new SaveFileDialog();
+			if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				path = save.FileName;
+				StreamWriter write = new StreamWriter(File.Create(path));
+				Note.Text = "";
+				write.Close();
+				FilePath.Text = path;
+				saved = true;
+			}
+			loading.Value = 0;
+		}
+
+		private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			loading.Value = 100;
+			OpenFileDialog open = new OpenFileDialog();
+			if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				path = open.FileName;
+				StreamReader reader = new StreamReader(path);
+				string data = reader.ReadToEnd();
+				Note.Text = data;
+				reader.Close();
+				FilePath.Text = path;
+			}
+			loading.Value = 0;
+		}
+
+		private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			loading.Value = 100;
+			SaveFileDialog save = new SaveFileDialog();
+			if (path == "")
+			{
+				if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				{
+					path = save.FileName;
+					StreamWriter writer = new StreamWriter(File.Create(path));
+					writer.Write(Note.Text);
+					writer.Flush();
+					writer.Close();
+					FilePath.Text = path;
+					saved = true;
+				}
+			}
+			else
+			{
+				StreamWriter writer = new StreamWriter(File.Create(path));
+				writer.Write(Note.Text);
+				writer.Flush();
+				writer.Close();
+				saved = true;
+			}
+			loading.Value = 0;
+		}
+
+		private void saveAsToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog save = new SaveFileDialog();
+			if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				path = save.FileName;
+				StreamWriter writer = new StreamWriter(File.Create(path));
+				writer.Write(Note.Text);
+				writer.Flush();
+				writer.Close();
+				FilePath.Text = path;
+				saved = true;
+			}
+		}
+
+		private void Note_TextChanged(object sender, EventArgs e)
+		{
+			saved = false;
+		}
+		public string getUserString()
+		{
+			return Note.Text;
+		}
+
+		private void NoteDock_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (!saved)
+			{
+				string message = "Save All Changes?";
+				string title = "NoteDock";
+				MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+				DialogResult result = MessageBox.Show(message, title, buttons);
+				if(result == DialogResult.Yes)
+				{
+					SaveFileDialog save = new SaveFileDialog();
+					if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+					{
+						path = save.FileName;
+						StreamWriter writer = new StreamWriter(File.Create(path));
+						writer.Write(Note.Text);
+						writer.Flush();
+						writer.Close();
+						FilePath.Text = path;
+						saved = true;
+					}
+				}
+			}
+		}
+	}
+}
